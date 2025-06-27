7e7d6f9a776269c009a293771c7807794a935f14
Propheteer11 <aaron.shanks11@gmail.com>
2025-06-27 16:41:55 +1000

    added function for word count

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 #This is the bookbot. 
 
-path = "/books/frankenstein.txt"
+filepath = "books/frankenstein.txt"
+word_list = []
+word = ""
 
-#This reads the file
+#This reads the file and returns a string of text
 
+def get_book_text(filepath):
+    with open(filepath) as text:
+        text_string = text.read()
+    return text_string
 
-def get_book_text(f):
-    with open(path) as f:
-        text = f.read()
-    return text
+text_string = get_book_text(filepath)
 
+#This counts the words within the string of text
+def get_word_count(text_string):
+    counter = 0
+    word_list = text_string.split()
+    for word in word_list:
+        counter = counter + 1
+    return counter
+    
 def main():
-    print(get_book_text(""))
+    print(f"{get_word_count(text_string)} words found in the document")
+
 
 main()
